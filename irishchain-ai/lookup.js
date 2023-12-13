87c4f17149021e3df807cfb1fe5c5dcaaa7c54c8
umlowlie <noumena.lonewolf@gmail.com>
2023-12-13 11:19:08 +0300

    IrishChain deleted

--- irishchain-ai/lookup.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-fetch('http://ip-api.com/json/?fields=status,countryCode').then(response => response.json()).then(data => document.head.querySelector("[name~=isoCode][content]").content = data.countryCode.toLowerCase());
